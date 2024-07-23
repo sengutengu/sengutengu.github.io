4a47e7154bbf497beaacb19b8baa8f4b31b42c9c
sengutengu <seungkim.h@gmail.com>
2024-07-23 10:42:12 -0400

    Resolve Leaflet blurry map issue

--- pages/chrysomallon/script.js
+++ pages/chrysomallon/script.js
@@ -2,10 +2,18 @@
 /* https://www.youtube.com/watch?v=d7wTA9F-l8c */
 
 // Set up map - Leaflet.js
-var esri_url ='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
-var esri_attribution = '© Esri © OpenStreetMap Contributors';
 
-var lyr_satellite = L.tileLayer(esri_url, {id: 'MapID', maxZoom: 20, tileSize: 512, zoomOffset: -1, attribution: esri_attribution});
+const map = new L.Map('map', {
+	layers: [
+		new L.TileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+			'attribution': '© Esri © OpenStreetMap Contributors'
+		})
+	],
+	center: [5.236302, 79.006576],
+	zoom: 2.5
+});
+
+// locations on map
 var indian_ocean = L.marker([-20.424956, 72.565296], {draggable:'false'}).bindPopup('<b>Indian Ocean</b>');
 var kairei = L.marker([-25.319444, 70.04], {draggable:'false'}).bindPopup('<b>Kairei</b>');
 var solitaire = L.marker([-19.5450, 65.8500], {draggable:'false'}).bindPopup('<b>Solitaire</b>');
@@ -13,13 +21,6 @@ var longqi  = L.marker([-37.7838, 49.6494], {draggable:'false'}).bindPopup('<b>L
 var kairei_pop = L.circle([-25.319444, 70.04], {color: "red",fillColor: "#f03",fillOpacity: 0.5,radius: 500000});
 var solitaire_pop = L.circle([-19.5450, 65.8500], {color: "red",fillColor: "#f03",fillOpacity: 0.5,radius: 500000});
 var longqi_pop = L.circle([-37.7838, 49.6494], {color: "blue",fillColor: "#06f",fillOpacity: 0.5,radius: 500000});
-
-var map = L.map('map', {
-    center: [5.236302, 79.006576],
-    zoom: 2.5,
-    layers: [lyr_satellite]
-});
-
 
 //const steps = d3.selectAll(".step");
 var main = document.querySelector("body");
